157bbcd042207dfa6794fac9c4f2c3be8e341e97
Burturo <bamahamaneiro68@gmail.com>
2025-08-31 17:22:47 +0100

    first commit

---
+++ src/main/java/com/example/medi_thech/MediThechApplication.java
@@ -0,0 +1,13 @@
+package com.example.medi_thech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediThechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediThechApplication.class, args);
+	}
+
+}
